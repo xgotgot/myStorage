b6c56fabfb6b9b98f76594fa5d86643f082cb58b
Matthieu Gauthier <matthieu.gauthier@bewave.io>
2020-01-18 15:25:29 -0500

    feathers and vue up

---
+++ frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn :to="{ name: 'login' }" text>
+        <span class="mr-2">Login</span>
+      </v-btn>
+      <v-btn :to="{ name: 'signup' }" text>
+        <span class="mr-2">Signup</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
